46d72b3f2d8091b2d9fb87e8b4c524716856e4b6
MJ <MJaSchnee@gmail.com>
2025-08-03 22:33:11 -0400

    Rename program and add plugin info

---
+++ WagerSpell/WagerSpell.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace WagerSpell
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class WagerSpell : BaseUnityPlugin
+    {
+        internal static new ManualLogSource Logger;
+
+        private void Awake()
+        {
+            Logger = base.Logger;
+            Logger.LogInfo($"Initializing {PluginInfo.PLUGIN_GUID}...");
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
