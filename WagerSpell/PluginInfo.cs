49df4630396e8769e72bbdd8cfae19bb5b87f946
MJ <MJaSchnee@gmail.com>
2025-08-04 09:07:08 -0400

    Update plugin name

--- WagerSpell/PluginInfo.cs
+++ WagerSpell/PluginInfo.cs
@@ -3,7 +3,7 @@
     internal class PluginInfo
     {
         public const string PLUGIN_GUID = "com.YeahThatsMJ.WagerSpell";
-        public const string PLUGIN_NAME = "Spell with a 50/50 chance to hit you instead of your target";
+        public const string PLUGIN_NAME = "Wager Spell";
         public const string PLUGIN_VERSION = "0.0.0";
     }
 }
